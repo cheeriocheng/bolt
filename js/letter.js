b59e7378487ef96a2d1258acaa5456c46d626e16
cheeriocheng <cheeriocheng@gmail.com>
2017-10-16 00:54:41 -0700

    cappie knolled

--- js/letter.js
+++ js/letter.js
@@ -15,40 +15,25 @@ class Letter {
         this.group = new THREE.Group();
         scene.add(this.group);
 
-
-
-        //This will add a starfield to the background of a scene
-        var starsGeometry = new THREE.Geometry();
-
+        var dotsGeometry = new THREE.Geometry();
         for ( var i = 0; i < this.totalDots; i ++ ) {
-
-            var star = new THREE.Vector3();
-            // var r = 70 + this.ind * 5;  //THREE.Math.randFloatSpread( 2000 );
-            // var theta= degToRad(i*20+this.ind);
-            // var phi = degToRad(this.totalDots + i * 10 );
-            var r = 70 + this.ind * 5;  //THREE.Math.randFloatSpread( 2000 );
+            var dot = new THREE.Vector3();
+            var r = 70 + this.ind * 5; 
             var theta= degToRad(i*5*this.ind);
             var phi = degToRad( this.totalDots%5*i );
             var spherical = new THREE.Spherical(r, phi, theta) ;
-            star.setFromSpherical( spherical );
-            starsGeometry.vertices.push(  star);
-            // debugger
+            dot.setFromSpherical( spherical );
+            dotsGeometry.vertices.push(dot);
           }
 
-        var starsMaterial = new THREE.PointsMaterial( { color: 0x880000, size:20 } );
+        var dotsMaterial = new THREE.PointsMaterial( { color: 0x880000, size:20 } );
+        this.group.add( new THREE.Points( dotsGeometry, dotsMaterial));
 
-        var starField = new THREE.Points( starsGeometry, starsMaterial );
-
-       
-        this.group.add( starField );
-            
         this.animateAppear();
 
     }
 
-    animateAppear(){
-        //this.group.scale = new THREE.Vector3(0,0,0);
-
+    animateAppear() {
         new TWEEN.Tween( this.group.scale ).to( {
             x: 2,
             y: 2,
@@ -62,15 +47,6 @@ class Letter {
   // Getter
   get letter() {
     return this.char();
-  }
-
-  // Method
-  // calcArea() {
-  //   return this.height * this.width;
-  // }
-
-
-  
- 
+  } 
 }
 
